22959b198d63dc7074d4057bf7ade90c71e9e767
Marco Pastene <mpastene1996@outlook.com>
2025-05-02 21:22:21 +0000

    marcopastene13

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-import "bootstrap";
-import "./style.css";
+let pron = ['el', 'nuestro', 'los']
+let nom = ['perro', 'gato', 'humano']
+let adj = ['gigante', 'pequeño', 'rojo']
 
+for (let i = 0; i < pron.length; i++) {
+  for (let n = 0; n < nom.length; n++) {
+    for (let a = 0; a < adj.length; a++){
+      console.log(pron[i] + nom[n] + adj[a] + ".com" );
+    }
+  }
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
